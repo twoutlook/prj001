e467f5dd2da63b5f98c6f74dce0191f8b38ab67e
Mark <twcloudwebapp@outlook.com>
2017-02-04 11:27:31 +0800

    fix the bug

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,8 +10,14 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    # def was_published_recently(self):
+    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    # https://docs.djangoproject.com/en/1.10/intro/tutorial05/
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
